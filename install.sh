d8b3642a40d2690e15dd4a0bc81f1fe5679457a2
Thomas Tuilier <ttuilier@enseirb-matmeca.fr>
2024-03-12 23:21:27 +0100

    Bug - verification de l'existance du fichier .env lors de l'installation

--- install.sh
+++ install.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
 # Modules a installer
+echo "Installation des modules python."
 pip install --upgrade openai
-pip install python-dotenv
+pip install --upgrade python-dotenv
 
 # Fichier pour la clé API d'OpenAI
-touch .env 
-echo "OPENAI_API_KEY = 'your-api-key-here'" >> .env
 
+fichierEnv=".env"
+echo "Creation du fichier .env"
+if [ -e "$fichierEnv" ];
+then
+	echo "--Le fichier .env existe déjà."
+else
+	touch .env
+	echo "OPENAI_API_KEY = 'your-api-key-here'" >> .env
+	echo "--Creation : OK"
 
+fi
