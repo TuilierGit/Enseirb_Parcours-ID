e384aba25d36038ed46ee59fb3d8fadadaacf9fa
Thomas Tuilier <ttuilier@enseirb-matmeca.fr>
2024-03-27 17:36:46 +0100

    Correction de make framactest

--- test/swap.c
+++ test/swap.c
@@ -1,7 +1,7 @@
 void swap(int *a,int *b)
 {
-int tmp = *a ;
-*a = *b ;
-*b = tmp ;
-return ;
+  int tmp = *a ;
+  *a = *b ;
+  *b = tmp ;
+  return ;
 }
--- test/swap1.h
+++ test/swap1.h
@@ -1,4 +1,5 @@
-/*@ ensures A: *a == \old(*b) ;
-@ ensures B: *b == \old(*a) ;
-@*/
-void swap(int *a,int *b) ;
+/*@ ensures A: *a == \old(*b);
+  @ ensures B: *b == \old(*a);
+  @ assigns *a, *b;
+  @ */
+void swap(int *a, int *b);
